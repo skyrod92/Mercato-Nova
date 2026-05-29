36be6fca688e357c0bf59f4559a729c0c51a9a0a
LucasGF <lucas.golabfernandez@edu.ece.fr>
2026-05-29 10:27:47 +0200

    database.php

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'mercato_nova';
+$user = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die('Erreur de connexion à la base de données : ' . $e->getMessage());
+}
+?>
